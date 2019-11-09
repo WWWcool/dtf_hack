3ce56d50152742b332b5445fa48a1f5d52ce307a
Anton Shalgachev <anton.shalgachev@gmail.com>
2019-11-09 21:11:47 +0300

    use original gravity in trajectory preview

--- Assets/Scripts/Managers/GravityManager.cs
+++ Assets/Scripts/Managers/GravityManager.cs
@@ -9,8 +9,13 @@ namespace UnityPrototype
         [SerializeField] private bool m_overrideGravity = false;
         [SerializeField] private Vector2 m_defaultGravity = Vector2.zero;
 
+        private Vector2 m_originalGravity;
+        public Vector2 originalGravity => m_originalGravity;
+
         private void Start()
         {
+            m_originalGravity = Physics2D.gravity;
+
             if (m_overrideGravity)
                 Physics2D.gravity = m_defaultGravity;
         }
