b5d6354b93e1779deb073a7d64abea182ec35014
Daniil Lebed <lebeddaniil@gmail.com>
2019-11-09 20:50:30 +0300

    Level_edgeTeleportation

---
+++ Assets/Scripts/Controls/EdgeTeleportation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityPrototype
+{
+    public class EdgeTeleportation : MonoBehaviour
+    {
+        [SerializeField] private bool m_canBeEdgeTeleported = false;
+
+        private void Update()
+        {
+            if (m_canBeEdgeTeleported)
+            {
+                if (transform.position.x>9.4f || transform.position.x < -9.4f)
+                    transform.position = new Vector2(-transform.position.x,
+                        transform.position.y);
+            }
+        }
+    }
+}
