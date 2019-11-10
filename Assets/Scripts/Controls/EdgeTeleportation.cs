02365004f4d1111f5d708e72aee5c56d4e507987
Daniil Lebed <lebeddaniil@gmail.com>
2019-11-10 20:20:07 +0300

    teleport fix

--- Assets/Scripts/Controls/EdgeTeleportation.cs
+++ Assets/Scripts/Controls/EdgeTeleportation.cs
@@ -7,15 +7,25 @@ namespace UnityPrototype
     public class EdgeTeleportation : MonoBehaviour
     {
         [SerializeField] private bool m_canBeEdgeTeleported = false;
+        private bool m_teleportReset = true;
 
         private void Update()
         {
-            if (m_canBeEdgeTeleported)
+            if (m_canBeEdgeTeleported&&m_teleportReset)
             {
                 if (transform.position.x>9.4f || transform.position.x < -9.4f)
-                    transform.position = new Vector2(-transform.position.x,
-                        transform.position.y);
+                {
+                    transform.position = new Vector2(-transform.position.x, transform.position.y);
+                    StartCoroutine(TeleportResetCorutine());
+                } 
             }
+        }
+
+        private IEnumerator TeleportResetCorutine()
+        {
+            m_teleportReset = false;
+            yield return new WaitForSeconds(0.2f);
+            m_teleportReset = true;
         }
     }
 }
